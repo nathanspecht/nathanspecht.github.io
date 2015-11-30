7bea592eb77483e9e7969bf5391987df7a504a9a
Nathan Specht <nathanspecht@gmail.com>
2015-11-30 12:37:29 -0800

    Add github links

--- components/project_thumb.js.jsx
+++ components/project_thumb.js.jsx
@@ -1,19 +1,40 @@
 (function(root) {
   'use strict';
   root.ProjectThumb = React.createClass({
+    getInitialState: function() {
+      return {linksHidden: " hidden"};
+    },
+
+    showLinks: function() {
+      this.setState({linksHidden: ""});
+    },
+
+    hideLinks: function() {
+      this.setState({linksHidden: " hidden"});
+    },
+
     render: function() {
       var divStyle = {
         backgroundImage: "url(" + this.props.project.imageUrl + ")"
       };
 
       return(
-        <div id="project-thumb">
+        <div id="project-thumb"
+             onMouseEnter={this.showLinks}
+             onMouseLeave={this.hideLinks}>
           <a href={this.props.project.liveLink}>
             <div style={divStyle} id="project-image"></div>
           </a>
           <h3>{this.props.project.name}</h3>
           <div id="project-description">
             {this.props.project.description}
+            <br></br>
+            <br></br>
+            <div className={"links" + this.state.linksHidden}>
+              <a href={this.props.project.liveLink}>live </a>
+               |
+              <a href={this.props.project.github}> git</a>
+            </div>
           </div>
         </div>
       );
--- components/projects_section.js.jsx
+++ components/projects_section.js.jsx
@@ -2,15 +2,26 @@
   'use strict';
 
   root.ProjectsSection = React.createClass({
+
+    getInitialState: function() {
+      return({showProjects: false});
+    },
+
+    toggleProjects: function() {
+      this.setState({showProjects: !this.state.showProjects});
+    },
+
     render: function() {
       return (
         <div id="projects-section">
-        <h2>Projects</h2>
-        {
-          root.PROJECTS.map(function(project) {
-            return <ProjectThumb project={project} />;
-          })
-        }
+        <h2 onClick={this.toggleProjects}>Projects</h2>
+          <div>
+            {
+              root.PROJECTS.map(function(project) {
+                return <ProjectThumb project={project} key={project.title}/>;
+              })
+            }
+          </div>
         </div>
       );
     }
